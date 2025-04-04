903a59a52d015cc639bc5868ff2872c083ed4153
JERRYRICHMAN007 <jerryrichman70@gmail.com>
2025-04-04 14:08:09 +0000

    RESET

---
+++ styles/auth.styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: "80%",
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    width: "80%",
+    marginTop: 10,
+  },
+});
