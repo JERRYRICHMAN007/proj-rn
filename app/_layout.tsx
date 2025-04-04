b9b302b4552394a73effc52940f12d8b5e639840
JERRYRICHMAN007 <jerryrichman70@gmail.com>
2025-04-04 01:43:15 +0000

    RESET

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Link, Stack } from "expo-router";
+import { Stack } from "expo-router";
 
-export default function Rootlayout() {
-  return <Stack />;
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="splash" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
